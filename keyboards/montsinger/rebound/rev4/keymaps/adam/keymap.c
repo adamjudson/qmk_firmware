ef2d153586aee3c6aaa73a0041c3285944dfb377
Adam <adamjudson@gmail.com>
2021-06-06 01:24:37 -0400

    dropped esc combo for _33 because lots of words have we in them.  Changed backspace to _trns from _no on num layer because I want it to work...

--- keyboards/montsinger/rebound/rev4/keymaps/adam/keymap.c
+++ keyboards/montsinger/rebound/rev4/keymaps/adam/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 	[5] = LAYOUT_all(
-    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                         KC_PAST, KC_7, KC_8, KC_9, KC_PMNS, KC_NO, 
+    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                         KC_PAST, KC_7, KC_8, KC_9, KC_PMNS, KC_TRNS, 
     KC_NO, KC_NO, KC_TRNS, KC_NO, KC_LGUI, KC_NO,                  KC_TRNS,                           KC_PMNS, KC_4, KC_5, KC_6, KC_PPLS, KC_NO, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                      KC_TRNS,                           KC_DOT, KC_1, KC_2, KC_3, KC_SLSH, KC_NO, 
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL,                    KC_TRNS,                           KC_0, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
