2bb6ca08e740fb329f4faa7363608abb3b95d47f
Adam <adamjudson@gmail.com>
2021-06-14 01:34:41 -0400

    mo(1) on A on ap2

--- keyboards/yd60mq/keymaps/adam/keymap.c
+++ keyboards/yd60mq/keymaps/adam/keymap.c
@@ -14,10 +14,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // lower
 	[1] = LAYOUT_all(
         KC_TILD, KC_F1,    KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,  KC_F12,   _______,  KC_DEL,
-        KC_TILD, KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,
+        KC_TILD, KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MPLY,    KC_TRNS, KC_TRNS,  KC_TRNS,
         KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_UNDS,    KC_PLUS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,
-        KC_TRNS, _______,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MUTE,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, TG(6),
-        KC_TRNS, KC_TRNS,  KC_TRNS,                             _______,    KC_SPC,     _______,                           KC_TRNS,    MO(3), KC_TRNS,  KC_TRNS,  KC_TRNS
+        KC_TRNS, _______,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MUTE,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_VOLU, TG(6),
+        KC_TRNS, KC_TRNS,  KC_TRNS,                             _______,    KC_SPC,     _______,                           KC_TRNS,    MO(3), KC_TRNS,  KC_VOLD,  KC_MFFD
     ),
 
     // raise
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[4] = LAYOUT_all(
         KC_TRNS, KC_TRNS,             KC_TRNS,              KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  _______, RESET,
         KC_TRNS, LGUI(LCTL(KC_LEFT)), LGUI(LCTL(KC_RIGHT)), LGUI(KC_E), LALT(KC_F4),   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,  RESET,
-        KC_TRNS, KC_TRNS,             KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS, MO(1),               KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
         KC_TRNS, _______,             KC_TRNS,    KC_TRNS,  KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,             KC_TRNS,                           _______,      KC_TRNS,    _______,                            KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS
     ),
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  _______, KC_TRNS,
         KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_PAST,    KC_7,       KC_8,       KC_9,       KC_PMNS,    KC_TRNS, KC_TRNS,  KC_TRNS,
         KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_LGUI,    KC_TRNS,    KC_PMNS,    KC_4,       KC_5,       KC_6,       KC_PPLS,    KC_TRNS, KC_TRNS,  KC_TRNS,
-        KC_TRNS, _______,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_DOT,     KC_1,       KC_2,       KC_3,       KC_SLSH, KC_TRNS,  KC_TRNS, KC_TRNS,
+        KC_TRNS, _______,  KC_TRNS,    KC_TRNS,    KC_LCTL,    KC_TRNS,    KC_TRNS,    KC_DOT,     KC_1,       KC_2,       KC_3,       KC_SLSH, KC_TRNS,  KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS,  KC_TRNS,                            _______,    KC_0,       _______,                            KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS
     ),
 
