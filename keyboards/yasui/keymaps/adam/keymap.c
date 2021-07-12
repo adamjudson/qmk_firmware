b6006bde46183daa3558be408b0c6ab5872463bd
Adam Judson <adamjudson@gmail.com>
2021-07-12 10:27:51 -0400

    drop shift on H because it was causing problems, though only for this keyboard

--- keyboards/yasui/keymaps/adam/keymap.c
+++ keyboards/yasui/keymaps/adam/keymap.c
@@ -5,12 +5,16 @@
 
 // I only have 8 keys on the bottom row, so middle two are KC_NO
 
+// H as shift is causing issues - not really clear why it's happenning only on this keyboard though
+// but I don't really use that shift, so taking it out for now
+// LSFT_T(KC_H) => KC_H
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* 0 - base */
 	LAYOUT(
         KC_Q,         KC_W,       KC_E,       KC_R,         KC_T,              KC_Y,         KC_U,       KC_I,         KC_O,    KC_P, 
-        LT(4,KC_A),   LT(5,KC_S), LT(8,KC_D), LSFT_T(KC_F), LT(6,KC_G),        LSFT_T(KC_H), LT(7,KC_J), LT(9,KC_K),  KC_L,     KC_SCLN, 
+        LT(4,KC_A),   LT(5,KC_S), LT(8,KC_D), LSFT_T(KC_F), LT(6,KC_G),        KC_H,         LT(7,KC_J), LT(9,KC_K),  KC_L,     KC_SCLN, 
         KC_Z,         KC_X,       KC_C,       KC_V,         KC_B,              KC_N,         KC_M,       KC_COMM,      KC_DOT,  KC_SLSH,  
         KC_LCTL,      KC_LGUI,    KC_LALT,  LT(1,KC_ENT),   KC_NO,             KC_NO,     LSFT_T(KC_SPC),   MO(2),    KC_ESC,   KC_BSPC   
     ), 
@@ -44,10 +48,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ), 
     /* 5 - s numbers */ 
 	LAYOUT(
-        KC_NO,    KC_NO,      KC_NO,      KC_NO,        KC_NO,         KC_PAST,      KC_P7,      KC_P8,          KC_P9,    KC_PMNS,   
-        KC_NO,    KC_TRNS,    KC_NO,      KC_TRNS,      KC_UNDS,       KC_PMNS,      KC_P4,      KC_P5,          KC_P6,    KC_PPLS,   
-        KC_NO,    KC_NO,      KC_NO,      KC_LGUI,      KC_NO,         KC_PDOT,      KC_P1,      KC_P2,          KC_P3,    KC_TRNS,
-        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_LCTL,      KC_NO,         KC_NO,        KC_P0,      KC_TRNS,        KC_NO,    KC_NO     
+        KC_NO,    KC_NO,      KC_NO,      KC_NO,        KC_NO,         KC_PAST,      KC_7,      KC_8,           KC_9,    KC_PMNS,   
+        KC_NO,    KC_TRNS,    KC_NO,      KC_TRNS,      KC_UNDS,       KC_PMNS,      KC_4,      KC_5,           KC_6,    KC_PPLS,   
+        KC_NO,    KC_NO,      KC_NO,      KC_LGUI,      KC_NO,         KC_PDOT,      KC_1,      KC_2,           KC_3,    KC_TRNS,
+        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_LCTL,      KC_NO,         KC_NO,        KC_0,      KC_TRNS,        KC_NO,    KC_NO     
     ),
     /* 6 - g brackets */ 
 	LAYOUT(
