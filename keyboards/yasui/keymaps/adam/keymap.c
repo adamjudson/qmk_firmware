f4a661ddbd583f11b483bd6991267e6160dfcb67
Adam Judson <adamjudson@gmail.com>
2021-07-14 21:07:40 -0400

    typo

--- keyboards/yasui/keymaps/adam/keymap.c
+++ keyboards/yasui/keymaps/adam/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,          KC_F12,       KC_MUTE,    KC_MPLY,     KC_PIPE, KC_BSLS,     
         KC_TRNS,      KC_TRNS,   KC_TRNS,     MO(3),        KC_NO,           KC_NO,        KC_TRNS,    KC_TRNS,     KC_VOLD,    KC_VOLU
     ), 
-    /* 3 - adjust  - this is pretty much only reset  and c-a-d  */  \
+    /* 3 - adjust  - this is pretty much only reset  and c-a-d  */ 
 	LAYOUT(
         KC_NO,        KC_NO,     KC_NO,       KC_NO,        KC_NO,             KC_NO,        KC_NO,    KC_NO,        KC_NO,   RESET, 
         KC_NO,        KC_NO,     KC_NO,       KC_NO,        KC_NO,             KC_NO,        KC_NO,    KC_NO,        KC_NO,   RESET, 
