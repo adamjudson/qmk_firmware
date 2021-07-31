1cdac5cffa9b7a1004845d02b97a23d493072b9c
Adam <adamjudson@gmail.com>
2021-07-30 23:24:14 -0400

    typos

--- keyboards/underscore33/rev2/keymaps/adam/keymap.c
+++ keyboards/underscore33/rev2/keymaps/adam/keymap.c
@@ -52,21 +52,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_33_split_space(\
       KC_Q,         KC_W,       KC_E,       KC_R,         KC_T,              KC_Y,         KC_U,       KC_I,         KC_O,     KC_P, \
       LT(4,KC_A),   LT(5,KC_S), LT(9,KC_D), LSFT_T(KC_F), LT(7,KC_G),        LSFT_T(KC_H), LT(8,KC_J), LT(10,KC_K),  KC_L,     KC_SCLN, \
-      KC_Z,         KC_X,       KC_C,       KC_V,         LT(7,KC_B),        KC_N,         KC_M,       KC_COMM,      KC_DOT,   KC_SLSH,  \
+      KC_Z,         KC_X,       KC_C,       KC_V,         KC_B,              KC_N,         KC_M,       KC_COMM,      KC_DOT,   KC_SLSH,  \
                         KC_LCTL,      KC_LGUI,   LT(1,KC_ENT),   LSFT_T(KC_SPC),   MO(2),    KC_ESC             ), \
   /* lower  - things to find a space for KC_TILD, KC_PIPE KC_QUOT again*/ \
 	[1] = LAYOUT_33_split_space(\
       KC_EXLM,      KC_AT,     KC_HASH,     KC_DLR,       KC_PERC,        KC_CIRC,      KC_AMPR,    KC_ASTR,      KC_DEL,  KC_BSPC,  \
       KC_F1,        KC_F2,     KC_F3,       KC_F4,        KC_F5,          KC_F6,        KC_UNDS,    KC_EQUAL,     KC_QUOT, LSFT(KC_QUOT), \
-      KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,         KC_F12,       KC_MUTE,    KC_LT,        KC_GT,    KC_QUES,  \
+      KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,         KC_F12,       KC_MUTE,    KC_LT,        KC_PIPE,  KC_BSLS, 
                          KC_TRNS,   KC_TRNS,     KC_TRNS,         LALT(KC_LEFT),  MO(3),         LALT(KC_RGHT)), \
 /* raise - KC_MPRV media previous*/ \
 \
 	[2] = LAYOUT_33_split_space(\
-      KC_1,         KC_2,      KC_3,        KC_4,         KC_5,            KC_6,         KC_7,       KC_8,          KC_9,    KC_0, \
-      KC_F1,        KC_F2,     KC_F3,       KC_F4,        KC_F5,           KC_F6,        KC_PMNS,    KC_EQUAL,      KC_QUOT, LSFT(KC_QUOT), \
-      KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,          KC_F12,       KC_MUTE,    KC_MPLY,     KC_PIPE, KC_BSLS,     \
-                           KC_GRAVE, KC_TILD,     MO(3),              KC_VOLD,      KC_TRNS,     KC_VOLU), \
+      KC_GRAVE,     KC_TILD,   KC_3,        LALT(KC_F4),   KC_5,           KC_6,         KC_7,       KC_8,          KC_9,    KC_MPLY, 
+      KC_F1,        KC_F2,     KC_F3,       KC_F4,        KC_F5,           KC_F6,        KC_PMNS,    KC_EQUAL,      KC_UP, LSFT(KC_QUOT), 
+      KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,          KC_F12,       KC_MUTE,    KC_LEFT,      KC_DOWN, KC_RIGHT,     
+                             KC_GRAVE, KC_TILD,     MO(3),              KC_VOLD,      KC_TRNS,     KC_VOLU), \
   /* adjust  - this is pretty much only reset  and c-a-d  */  \
 	[3] = LAYOUT_33_split_space(\
       KC_NO,        KC_NO,     KC_NO,       KC_NO,        KC_NO,             KC_NO,        KC_NO,     KC_NO,        KC_NO,   RESET, \
