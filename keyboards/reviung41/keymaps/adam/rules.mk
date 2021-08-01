38f231fdd6fbdce315c423f19f41e34b015e71f5
Adam <adamjudson@gmail.com>
2021-07-31 23:53:52 -0400

    panda 41

---
+++ keyboards/reviung41/keymaps/adam/rules.mk
@@ -0,0 +1,10 @@
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootload
+#   ATmega328P   USBasp
+BOOTLOADER = qmk-dfu
+COMBO_ENABLE = yes
