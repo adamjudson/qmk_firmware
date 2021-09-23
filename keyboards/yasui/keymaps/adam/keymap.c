50a9ca78eeb86e6e68291261e6d095ca79c32243
Adam Judson <adamjudson@gmail.com>
2021-09-22 23:17:53 -0400

    more planck stuff - TD(TD_MEDIA) does not work for some reason

--- keyboards/yasui/keymaps/adam/keymap.c
+++ keyboards/yasui/keymaps/adam/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 2 - raise - try to avoid putting anything important here */
 	LAYOUT(
         KC_GRAVE,     KC_TILD,   KC_3,        LALT(KC_F4),   KC_5,           KC_6,         KC_7,       KC_VOLD,       KC_VOLU,    TD(TD_MEDIA), 
-        KC_F1,        KC_F2,     KC_F3,       KC_F4,        KC_F5,           KC_F6,        KC_PMNS,    KC_EQUAL,      KC_UP, LSFT(KC_QUOT), 
+        KC_F1,        KC_F2,     KC_F3,       KC_F4,        KC_F5,           KC_F6,        KC_MINS,    KC_EQUAL,      KC_UP, LSFT(KC_QUOT), 
         KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,          KC_F12,       KC_MUTE,    KC_LEFT,      KC_DOWN, KC_RIGHT,     
         KC_TRNS,      KC_TRNS,   KC_TRNS,     MO(3),        KC_NO,           KC_NO,        KC_TRNS,    KC_TRNS,      KC_VOLD,    KC_VOLU
     ), 
