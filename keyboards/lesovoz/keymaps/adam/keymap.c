b7400ce51dd379c38d48df5389848645376d1990
Adam Judson <adamjudson@gmail.com>
2022-04-22 12:00:02 -0400

    fix for 2u key on lesovoz

--- keyboards/lesovoz/keymaps/adam/keymap.c
+++ keyboards/lesovoz/keymaps/adam/keymap.c
@@ -4,39 +4,39 @@
 #include "adam.h"
 
 // Currently I have 1,1,3,  2,1,1,1 on the bottom row.
-// which means keys 3/5 are dead.  And 6 (so 6 and 7 are duplicates)
+// which means keys 3/5 are dead.  And 7 (so 6 and 7 are duplicates)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* 0 - base */
 	LAYOUT(
-        KC_Q,         KC_W,       KC_E,       KC_R,         KC_T,              KC_Y,         KC_U,       KC_I,         KC_O,    KC_P, 
-        LT(4,KC_A),   LT(5,KC_S), LT(8,KC_D), LSFT_T(KC_F), LT(6,KC_G),      SFT_T(KC_H),   LT(7,KC_J), LT(9,KC_K),  KC_L,     KC_SCLN, 
-      MT(MOD_LCTL, KC_Z),    MT(MOD_LALT, KC_X),       KC_C,       KC_V,         KC_B,              KC_N,         KC_M,       KC_COMM,      KC_DOT,   MT(MOD_LGUI, KC_SLSH),  
-        KC_LCTL,      KC_LGUI,    KC_LALT,  LT(1,KC_ENT),   KC_NO,             LSFT_T(KC_SPC),     LSFT_T(KC_SPC),   MO(2),    KC_ESC,   KC_BSPC   
+        KC_Q,                KC_W,                  KC_E,       KC_R,         KC_T,       KC_Y,           KC_U,           KC_I,        KC_O,    KC_P, 
+        LT(4,KC_A),          LT(5,KC_S),            LT(8,KC_D), LSFT_T(KC_F), LT(6,KC_G), SFT_T(KC_H),    LT(7,KC_J),     LT(9,KC_K),  KC_L,    KC_SCLN, 
+        MT(MOD_LCTL, KC_Z),  MT(MOD_LALT, KC_X),    KC_C,       KC_V,         KC_B,       KC_N,           KC_M,           KC_COMM,     KC_DOT,  MT(MOD_LGUI, KC_SLSH),  
+        KC_LCTL,             KC_LGUI,               KC_LALT,    LT(1,KC_ENT), KC_NO,      LSFT_T(KC_SPC), LSFT_T(KC_SPC),  MO(2),       KC_ESC, KC_BSPC   
     ), 
     /* 1 - lower */
     // kept hitting F6 when I want underscore...
 	LAYOUT(
-        KC_EXLM,      KC_AT,     KC_HASH,     KC_DLR,       KC_PERC,        KC_CIRC,      KC_AMPR,    KC_ASTR,      KC_DEL,  KC_BSPC,  
-        KC_F1,        KC_F2,     KC_F3,       KC_F4,        TD(TD_F5_F6),   KC_UNDS,      KC_UNDS,    KC_EQUAL,     KC_QUOT, LSFT(KC_QUOT), 
+        KC_EXLM,      KC_AT,     KC_HASH,     KC_DLR,       KC_PERC,        KC_CIRC,      KC_AMPR,    KC_ASTR,      KC_DEL,   KC_BSPC,  
+        KC_F1,        KC_F2,     KC_F3,       KC_F4,        TD(TD_F5_F6),   KC_UNDS,      KC_UNDS,    KC_EQUAL,     KC_QUOT,  LSFT(KC_QUOT), 
         KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,         KC_F12,       KC_GRAVE,   KC_TILD,      KC_PIPE,  KC_BSLS, 
         KC_TRNS,      KC_TRNS,   KC_TRNS,     KC_TRNS,      KC_NO,          KC_TRNS,        KC_TRNS,    MO(3),      KC_GRAVE, KC_TILD
     ), 
     /* 2 - raise - try to avoid putting anything important here */
 	LAYOUT(
-        KC_GRAVE,     KC_TILD,   KC_3,        LALT(KC_F4),   KC_5,           KC_6,         KC_7,       KC_VOLD,       KC_VOLU,    TD(TD_MEDIA), 
-        KC_F1,        KC_F2,     KC_F3,       KC_F4,        KC_F5,           KC_F6,        KC_MINS,    KC_EQUAL,      KC_UP, LSFT(KC_QUOT), 
-        KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,          KC_F12,       KC_MUTE,    KC_LEFT,      KC_DOWN, KC_RIGHT,     
-        KC_TRNS,      KC_TRNS,   KC_TRNS,     MO(3),        KC_NO,           KC_TRNS,        KC_TRNS,    KC_TRNS,      KC_VOLD,    KC_VOLU
+        KC_GRAVE,     KC_TILD,   KC_3,        LALT(KC_F4),   KC_5,      KC_6,    KC_7,       KC_VOLD,   KC_VOLU,    TD(TD_MEDIA), 
+        KC_F1,        KC_F2,     KC_F3,       KC_F4,        KC_F5,      KC_F6,   KC_MINS,    KC_EQUAL,  KC_UP,      LSFT(KC_QUOT), 
+        KC_F7,        KC_F8,     KC_F9,       KC_F10,       KC_F11,     KC_F12,  KC_MUTE,    KC_LEFT,   KC_DOWN,    KC_RIGHT,     
+        KC_TRNS,      KC_TRNS,   KC_TRNS,     MO(3),        KC_NO,      KC_TRNS, KC_TRNS,    KC_NO,     KC_VOLD,    KC_VOLU
     ), 
     /* 3 - adjust  - this is pretty much only reset  and c-a-d  */ 
 	LAYOUT(
-        DYN_REC_START1, DYN_MACRO_PLAY1,  KC_NO,       KC_NO,        KC_NO,             KC_NO,        KC_NO,    KC_NO,        KC_NO,   RESET, 
-        DYN_REC_STOP,        TG(5),       KC_NO,       KC_NO,        KC_NO,             KC_NO,        RGB_TOG,  RGB_VAI,      RGB_VAD, RESET, 
-        KC_NO,                 KC_NO,     KC_NO,       KC_NO,        KC_NO,             KC_NO,        RGB_MOD,  RGB_HUI,      RGB_HUD, KC_NO, 
-        KC_NO,                 KC_NO,     KC_NO,       KC_TRNS,      KC_NO,             KC_TRNS,    LCA(KC_DEL),  KC_TRNS,      KC_NO,   KC_NO
+        DYN_REC_START1, DYN_MACRO_PLAY1,  KC_NO,       KC_NO,        KC_NO,    KC_NO,        KC_NO,    KC_NO,        KC_NO,   RESET, 
+        DYN_REC_STOP,        TG(5),       KC_NO,       KC_NO,        KC_NO,    KC_NO,        RGB_TOG,  RGB_VAI,      RGB_VAD, RESET, 
+        KC_NO,                 KC_NO,     KC_NO,       KC_NO,        KC_NO,    KC_NO,        RGB_MOD,  RGB_HUI,      RGB_HUD, KC_NO, 
+        KC_NO,                 KC_NO,     KC_NO,       KC_TRNS,      KC_NO,    LCA(KC_DEL),  KC_NO,    KC_TRNS,      KC_NO,   KC_NO
     ), 
     /* 4 - anne pro / desktop swap - dropped os-e shifted left and right  - a is not a great key to use.  Especially for alt-F4.  Moving that to k*/ 
     /* MO on s is questionable - triggering when I don't want sometimes */
@@ -44,42 +44,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO,    LGUI(LCTL(KC_LEFT)), LGUI(LCTL(KC_RIGHT)),   KC_TRNS,       KC_NO,   KC_NO,        KC_MS_WH_UP,         KC_MS_UP,      KC_MS_WH_DOWN,    RESET, 
        KC_NO,    MO(1),               MO(2),                  KC_ESC,        KC_NO,   KC_NO,        KC_MS_LEFT,          KC_MS_DOWN,    KC_MS_RIGHT,      KC_NO, 
        KC_NO,    KC_NO,               KC_NO,                  KC_NO,         KC_NO,   KC_NO,        KC_NO,               KC_NO,         KC_NO,            KC_NO, 
-       KC_NO,    KC_NO,               KC_NO,                  KC_MS_BTN1,    KC_NO,   KC_NO,        KC_MS_BTN1,          KC_MS_BTN2,    KC_VOLD,    KC_VOLU 
+       KC_NO,    KC_NO,               KC_NO,                  KC_MS_BTN1,    KC_NO,   KC_MS_BTN1,   KC_NO,               KC_MS_BTN2,    KC_VOLD,    KC_VOLU 
     ), 
     /* 5 - s numbers - and tab I guess */ 
 	LAYOUT(
-        KC_NO,              KC_NO,      KC_TRNS,      KC_NO,        KC_NO,         KC_PAST,      KC_7,      KC_8,           KC_9,    KC_MINS,  
-        OSM(MOD_LGUI),      TG(5),      KC_TAB,     KC_TRNS,      KC_UNDS,       KC_MINS,      KC_4,      KC_5,           KC_6,    KC_PLUS,   
-        KC_NO,              KC_NO,      KC_NO,      KC_LGUI,      KC_NO,         KC_PDOT,      KC_1,      KC_2,           KC_3,    KC_TRNS,
-        KC_TRNS,            KC_TRNS,    KC_TRNS,    KC_LCTL,      KC_NO,         KC_NO,        KC_0,      KC_TRNS,        KC_NO,    KC_NO     
+        KC_NO,              KC_NO,      KC_TRNS,      KC_NO,                KC_NO,         KC_ASTR,      KC_7,      KC_8,           KC_9,    KC_MINS,  
+        OSM(MOD_LGUI),      TG(5),      KC_TAB,     KC_TRNS,                KC_UNDS,       KC_MINS,      KC_4,      KC_5,           KC_6,    KC_PLUS,   
+        KC_NO,              KC_NO,      KC_NO,      KC_LGUI,                KC_NO,         KC_DOT,       KC_1,      KC_2,           KC_3,    KC_TRNS,
+        KC_TRNS,            KC_TRNS,    KC_TRNS,    MT(MOD_LCTL, KC_DOT),   KC_NO,         KC_0,         KC_NO,     KC_DOT,         KC_TRNS, KC_TRNS     
     ),
     /* 6 - g brackets */ 
 	LAYOUT(
         KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_NO,        KC_NO,                KC_NO,        ARRAY,      KC_LBRC,        KC_RBRC,  ARRAY_INSIDE,   
         KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_BSPC,      KC_NO,                JSARROW,      KC_LPRN,    KC_RPRN,        KC_LCBR,  KC_RCBR,   
         KC_TRNS,  KC_TRNS,    KC_NO,      KC_NO,        KC_NO,                KC_NO,        KC_LT,      KC_GT,          END_TAG,  KC_GT,     
-        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_NO,        KC_NO,                KC_TRNS,        KC_TRNS,    KC_NO,          KC_NO,    KC_NO   
+        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_NO,        KC_NO,                KC_TRNS,      KC_TRNS,    KC_NO,          KC_NO,    KC_NO   
     ), 
     /* 7 - j  os nav */ 
 	LAYOUT(
-        LGUI(KC_1),    LGUI(KC_2), LGUI(KC_E), LGUI(KC_4), LGUI(KC_5),      LGUI(KC_6),   LGUI(KC_7), LGUI(KC_8),     KC_NO,    KC_BSPC, 
-        OSM(MOD_LCTL), KC_TRNS,    KC_TRNS,    LCTL(KC_F),  KC_TAB,         KC_NO,        KC_TRNS,    KC_LGUI,        LGUI(KC_L), KC_LALT, 
-        KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,         KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_NO,    KC_NO, 
-        KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_BSPC,    KC_NO,           KC_TRNS,        KC_TRNS,    KC_NO,          KC_NO,    KC_NO
+        LGUI(KC_1),    LGUI(KC_2), LGUI(KC_E), LGUI(KC_4), LGUI(KC_5),      LGUI(KC_6),   LGUI(KC_7), LGUI(KC_8),     KC_NO,      KC_BSPC, 
+        OSM(MOD_LCTL), KC_TRNS,    KC_TRNS,    LCTL(KC_F), KC_TAB,          KC_NO,        KC_TRNS,    KC_LGUI,        LGUI(KC_L), KC_LALT, 
+        KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,         KC_TRNS,      KC_TRNS,    KC_TRNS,        KC_NO,      KC_NO, 
+        KC_TRNS,       KC_TRNS,    KC_TRNS,    KC_BSPC,    KC_NO,           KC_TRNS,      KC_TRNS,    KC_NO,          KC_NO,      KC_NO
     ), 
     /* 8 - d nav */ 
 	LAYOUT(
         KC_TRNS,  LCTL(KC_W), KC_TRNS,    LCTL(KC_R),   LCTL(KC_T),       KC_TRNS,      KC_PGUP,     KC_UP,         KC_PGDN,            KC_BSPC, 
         KC_ESC,   KC_DEL,     KC_TRNS,    KC_LSFT,      LCTL(KC_TAB),     KC_HOME,      KC_LEFT,     KC_DOWN,       KC_RGHT,            KC_END, 
         KC_LALT,  KC_TRNS,    LCTL(KC_C), LCTL(KC_V),   KC_TRNS,          KC_TRNS,      LCTL(KC_U),  KC_BSPC,       LCTL(LSFT(KC_U)),   LCTL(KC_SLSH), 
-        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_LCTL,      KC_NO,            LCTL(KC_C),        LCTL(KC_C),  LCTL(KC_X),    LCTL(KC_U),         LCTL(LSFT(KC_U))
+        KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_LCTL,      KC_NO,            LCTL(KC_C),   KC_NO,       LCTL(KC_X),    LCTL(KC_U),         LCTL(LSFT(KC_U))
     ), 
     /* 9 - k ctrl */ 
 	LAYOUT(
-      KC_TAB,    LSFT(KC_TAB), KC_TRNS,    KC_TRNS,        KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,  
-      KC_ESC,    KC_TRNS,     KC_BSPC,     KC_BSPC,      LALT(KC_LEFT),     KC_TRNS,    KC_LCTL,    KC_NO,     LCTL(KC_L), OSM(MOD_LALT), 
-      KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_TRNS,        KC_NO,           KC_TRNS,     KC_NO,      KC_NO,     KC_NO,      LCTL(KC_SLSH), 
-      KC_TRNS,   KC_TRNS,     KC_TRNS,     KC_BSPC,      KC_NO,             KC_TRNS,      KC_TRNS,    KC_NO,     KC_TRNS,    KC_TRNS
+      KC_TAB,    LSFT(KC_TAB), KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,  
+      KC_ESC,    KC_TRNS,      KC_BSPC,  KC_BSPC,  LALT(KC_LEFT),  KC_TRNS,    KC_LCTL,    KC_NO,     LCTL(KC_L), OSM(MOD_LALT), 
+      KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_TRNS,  KC_NO,          KC_TRNS,    KC_NO,      KC_NO,     KC_NO,      LCTL(KC_SLSH), 
+      KC_TRNS,   KC_TRNS,      KC_TRNS,  KC_BSPC,  KC_NO,          KC_TRNS,    KC_TRNS,    KC_NO,     KC_TRNS,    KC_TRNS
     ) 
 };
 
