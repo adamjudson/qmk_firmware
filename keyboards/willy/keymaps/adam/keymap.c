975114419a6561a4a8b1726fd25e0942cd44394e
Adam Judson <adamjudson@gmail.com>
2022-12-02 17:59:07 -0500

    fix will keys that were one key left on free-willy

--- keyboards/willy/keymaps/adam/keymap.c
+++ keyboards/willy/keymaps/adam/keymap.c
@@ -44,20 +44,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT(
       DYN_REC_START1,      DYN_MACRO_PLAY1,     KC_NO,       KC_NO,        KC_NO,             KC_NO,        KC_NO,     KC_NO,        KC_NO,   RESET, 
       DYN_REC_STOP,        TG(5),               KC_NO,       KC_NO,        KC_NO,             KC_NO,        RGB_TOG,  RGB_VAI,      RGB_VAD, RESET, 
-      KC_NO,                KC_NO,              KC_NO,       KC_NO,        KC_NO,             KC_NO,         KC_NO,    KC_NO,        KC_NO,   KC_NO, 
-                                                KC_TRNS,     KC_TRNS,      KC_TRNS,      LCA(KC_DEL),         KC_TRNS,   KC_NO), 
+      KC_NO,                KC_NO,              KC_NO,       KC_NO,        KC_NO,             KC_NO,        KC_NO,    KC_NO,        KC_NO,   KC_NO, 
+                                                KC_TRNS,     KC_TRNS,      KC_TRNS,      LCA(KC_DEL),       LCA(KC_DEL),   KC_NO), 
     /* anne pro / desktop swap - dropped os-e shifted left and right  - a is not a great key to use.  Especially for alt-F4.  Moving that to k*/ 
 	[4] = LAYOUT(
       KC_NO,    LGUI(LCTL(KC_LEFT)), LGUI(LCTL(KC_RIGHT)),   KC_TRNS,       KC_NO,   KC_NO,        KC_MS_WH_UP,         KC_MS_UP,      KC_MS_WH_DOWN,    RESET, 
       KC_NO,    MO(1),               MO(2),                  KC_ESC,        KC_NO,   KC_NO,        KC_MS_LEFT,          KC_MS_DOWN,    KC_MS_RIGHT,      KC_NO, 
       KC_TRNS,    KC_NO,               KC_NO,                  KC_NO,         KC_NO,   KC_NO,        KC_NO,               KC_NO,         KC_NO,          KC_INS, 
-                                     KC_NO,                  KC_MS_BTN1,    KC_NO,   KC_MS_BTN1,   KC_NO,               KC_MS_BTN2 ), 
+                                     KC_NO,                  KC_MS_BTN1,    KC_MS_BTN1,   KC_MS_BTN1,   KC_NO,               KC_MS_BTN2 ), 
     /* numbers */ 
 	[5] = LAYOUT(
       KC_NO,            KC_NO,      KC_NO,      KC_NO,                    KC_NO,           KC_ASTR,      KC_7,      KC_8,     KC_9,    KC_BSPC,   
       OSM(MOD_LGUI),    TG(5),      KC_TAB,     KC_TRNS,                  KC_UNDS,         KC_MINS,      KC_4,      KC_5,     KC_6,    KC_PLUS,   
       KC_NO,            KC_NO,      KC_NO,      KC_LGUI,                  KC_NO,           KC_DOT,       KC_1,      KC_2,     KC_3,    KC_TRNS,   
-                                    KC_LALT,    MT(MOD_LCTL, KC_DOT),     KC_LCTL,         KC_0,         KC_0,      KC_DOT     ),
+                                    KC_LALT,    MT(MOD_LCTL, KC_DOT),     MT(MOD_LCTL, KC_DOT),          KC_0,      KC_0,      KC_DOT     ),
     /* brackets - g layer */ 
 	[6] = LAYOUT(
       KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_NO,        KC_NO,           KC_NO,        ARRAY,      KC_LBRC,        KC_RBRC,  ARRAY_INSIDE,  
