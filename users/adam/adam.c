cd74d53d804d6d77dca1510b37ccf494c498a63c
Adam Judson <adamjudson@gmail.com>
2024-06-27 12:50:05 -0400

    moved combos to adam.c - will break other keyboard builds

--- users/adam/adam.c
+++ users/adam/adam.c
@@ -31,6 +31,72 @@ bool process_record_user(uint16_t keycode, keyrecord_t * record) {
         return process_record_adam(keycode, record);
     }
 
+#ifdef COMBO_ENABLE
+// const uint16_t PROGMEM combo_cut[] = {KC_Z, KC_X, COMBO_END};
+const uint16_t PROGMEM combo_esc[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM combo_cutt[] = {MT(MOD_LCTL, KC_Z), MT(MOD_LALT, KC_X), COMBO_END};
+const uint16_t PROGMEM combo_bslash[] = {KC_DOT, MT(MOD_LGUI, KC_SLSH), COMBO_END};
+
+const uint16_t PROGMEM combo_copy[]         = {KC_X, KC_C, COMBO_END};
+const uint16_t PROGMEM combo_copyy[]        = {MT(MOD_LALT, KC_X), KC_C, COMBO_END};
+const uint16_t PROGMEM combo_paste[]        = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM combo_bspc[]         = {KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM combo_tab[]          = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_single_quote[] = {KC_L, LT(5, KC_SCLN), COMBO_END};
+// this doesn't seem to work - probably because f is already overloaded?
+// const uint16_t PROGMEM combo_another_bspc[] = {KC_F, KC_E, COMBO_END};
+// const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
+
+// enum combo_events { ZX_CUT, ZX_CUTT, XC_COPY, XC_COPYY, CV_PASTE, DOTSLASH_BSLASH };
+enum combo_events { ZX_CUTT, XC_COPY, XC_COPYY, CV_PASTE, DOTSLASH_BSLASH };
+
+// don't think this worked when combo_esc was at the beginning
+combo_t key_combos[COMBO_COUNT] = {
+//     [ZX_CUT] = COMBO_ACTION(combo_cut),
+    // QWE_ESC] = COMBO_ACTION(combo_esc),
+    [ZX_CUTT] = COMBO_ACTION(combo_cutt),
+    [XC_COPY] = COMBO_ACTION(combo_copy),
+    [XC_COPYY] = COMBO_ACTION(combo_copyy),
+    [CV_PASTE] = COMBO_ACTION(combo_paste),
+    [DOTSLASH_BSLASH] = COMBO_ACTION(combo_bslash),
+    COMBO(combo_esc, KC_ESC),
+    COMBO(combo_bspc, KC_BSPC), 
+    COMBO(combo_tab, KC_TAB), 
+    COMBO(combo_single_quote, KC_QUOTE), 
+};
+
+// note that that this is using the index into key_combos, which is assumed to be the enum
+// anything that's not a simple keypress needs to be here
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch (combo_index) {
+        case CV_PASTE:
+            if (pressed) {
+                tap_code16(LCTL(KC_V));
+            }
+            break;
+        case XC_COPY:
+        case XC_COPYY:
+            if (pressed) {
+                tap_code16(LCTL(KC_C));
+            }
+            break;
+//         case ZX_CUT:
+        case ZX_CUTT:
+            if (pressed) {
+                tap_code16(LCTL(KC_X));
+            }
+            break;
+        case DOTSLASH_BSLASH:
+            if (pressed) {
+                tap_code16(KC_BSLASH);
+            }
+            break;
+    }
+}
+#endif 
+
+
+
 #ifdef TAP_DANCE_ENABLE
 
 void dance_media(qk_tap_dance_state_t *state, void *user_data) {
